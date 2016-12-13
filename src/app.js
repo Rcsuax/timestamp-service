83caef9f47ba800bc219d10636a9512bec249c7f
rcsuax <rcsuax@gmail.com>
2016-12-13 15:42:38 +0000

    beautified app.js

--- src/app.js
+++ src/app.js
@@ -1,37 +1,47 @@
 "use strict";
 
 import express from 'express';
-import moment from 'moment'
+import moment from 'moment';
 
 const app = express();
+let port = process.env.PORT || 8080;
 
+//This is to prevent the annoying favicon.ico browser requests
 app.get('/favicon.ico', (req, res) => {
     res.send(200);
 });
 
-app.get('/:time', (req,res) => {
-  let time = req.params.time
-  let unix = null;
-  let natural = null;
-  if (+time >= 0) {
-    unix = +time;
-    natural = unixToNat(unix);
-}
-// Check for initial natural time
-if (isNaN(+time) && moment(time, "MMMM D, YYYY").isValid()) {
-    unix = +natToUnix(time);
-    natural = unixToNat(unix);
-}
-
-  res.send({ "unix": unix, "natural": natural })
+app.get('/', (req,res) => {
+  res.send({"text":"timestamp home, input unix or natural time at after / to have it converted"})
 })
-let port = process.env.PORT || 8080;
+
+app.get('/:time', (req, res) => {
+    let time = req.params.time
+    let unix = null;
+    let natural = null;
+
+    if (+time >= 0) {
+        unix = +time;
+        natural = unixToNat(unix);
+    }
+
+    if (isNaN(+time) && moment(time, "MMMM D, YYYY").isValid()) {
+        unix = +natToUnix(time);
+        natural = unixToNat(unix);
+    }
+
+    res.send({
+        "unix": unix,
+        "natural": natural
+    })
+})
+
 app.listen(port, () => console.log('express running on port 3000'));
 
-function natToUnix(date) {
+function naturalToUnix(date) {
     return moment(date, "MMMM D, YYYY").format("X");
 }
 
-function unixToNat(unix) {
+function unixToNatural(unix) {
     return moment.unix(unix).format("MMMM D, YYYY");
 }
